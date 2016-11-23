61115b6605a919759dd26ff443dd779eaf36dbea
danie_000 <danie_000@Gabriela>
2016-11-22 22:06:20 -0500

    Cree un constructor para la clase mapa, recibiendo todos los personajes del juego, y el mapa donde se van a ubicar.Es necesario todo esto, ya que mapa sera nuestra clase principal por lo tanto contener el estado de todo el juego para aplicarle persistencia

--- HumansAnger/src/humanangers/Map.java
+++ HumansAnger/src/humanangers/Map.java
@@ -13,6 +13,61 @@ package humanangers;
 //En este metodo podria ingresar un heroe como tal ya que no es necesario que el vector 
 //completo se mueva, e igualmente en la batalla ya no moveremos los characters
 public class Map {
+    
+    private Character Hades;
+    private Character Cerberus;
+    private Character Baphomet;
+    private Character Ra;
+    private Character Anubis;
+    private Character Jesus;
+    private Character Vishnu;
+    private Character Arcangel;
+    private Character Vassago;
+    private Character Lilith;
+    private Character Ucrunius;
+    private Character Ganesha;
+    private Character Beleth;
+    private Character Bifrons;
+    private Character Emmao;
+    private Character Marduk;
+    private Character Set;
+    private Character hunterRobot;
+    private Character wizardRobot;
+    private Character warriorRobot;
+    private int[][] mapOfWorld;
+    
+    /**
+    *Vectores que se utilizaran para almacenar los heroes y enemigos que entraran a una batalla
+    * y poder ubicarlos en la grilla. No entran como elementos al mapa solo a los combates
+    */
+            
+    private Character[] Heroes;
+    private Character[] Enemys;       
+
+    public Map(Character Hades, Character Cerberus, Character Baphomet, Character Ra, Character Anubis, Character Jesus, Character Vishnu, Character Arcangel, Character Vassago, Character Lilith, Character Ucrunius, Character Ganesha, Character Beleth, Character Bifrons, Character Emmao, Character Marduk, Character Set, Character hunterRobot, Character wizardRobot, Character warriorRobot, int[][] mapOfWorld) {
+        this.Hades = Hades;
+        this.Cerberus = Cerberus;
+        this.Baphomet = Baphomet;
+        this.Ra = Ra;
+        this.Anubis = Anubis;
+        this.Jesus = Jesus;
+        this.Vishnu = Vishnu;
+        this.Arcangel = Arcangel;
+        this.Vassago = Vassago;
+        this.Lilith = Lilith;
+        this.Ucrunius = Ucrunius;
+        this.Ganesha = Ganesha;
+        this.Beleth = Beleth;
+        this.Bifrons = Bifrons;
+        this.Emmao = Emmao;
+        this.Marduk = Marduk;
+        this.Set = Set;
+        this.hunterRobot = hunterRobot;
+        this.wizardRobot = wizardRobot;
+        this.warriorRobot = warriorRobot;
+        this.mapOfWorld = mapOfWorld;
+        
+    }
 
     public void move(Character[] Heroes) {
 
